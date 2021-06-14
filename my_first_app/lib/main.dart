6e34640c72a4809204410bbd511a69e1c925006e
Ayishathrifa <rifasvlog@gmail.com>
2021-06-14 14:46:21 +0530

    changes applied

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,7 +1,25 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:my_first_app/screens/home_page.dart';
+import 'package:my_first_app/screens/profile_page.dart';
+import 'package:path_provider/path_provider.dart';
+import 'screens/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory appDocDir = await getApplicationDocumentsDirectory();
+  String appDocPath = appDocDir.path;
+  Hive.init(appDocPath);
+
+  // ignore: unused_local_variable
+  var boxS = await Hive.openBox('profile');
+  var box = Hive.box('profile');
+  box.put('name', 'RIFA');
+  var name = box.get('name');
+
+  print('Name: $name');
+
   runApp(MyApp());
 }
 
@@ -14,6 +32,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      routes: {ProfileScreen.routeName: (ctx) => ProfileScreen()},
       home: Homepage(),
     );
   }
--- my_first_app/lib/screens/home_page.dart
+++ my_first_app/lib/screens/home_page.dart
@@ -1,16 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:my_first_app/model/photos_model.dart';
+import 'package:my_first_app/screens/profile_page.dart';
 import 'package:my_first_app/screens/widget/top_bar.dart';
 import 'package:my_first_app/env/keys.dart' as config;
+import 'package:hive_flutter/hive_flutter.dart';
 
 // ignore: must_be_immutable
 class Homepage extends StatefulWidget {
   @override
-  _HomepageState createState() => _HomepageState();
+  _HomepageScreenState createState() => _HomepageScreenState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageScreenState extends State<Homepage> {
   List<PhotosModel> _photosData = [];
 
   Future<void> fetchphotos() async {
@@ -47,40 +50,52 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TopBar(
-              title: 'AYISHATH RIFA',
-              subtitle: 'Developer',
-              color: Color(0xff0B3D2E),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ValueListenableBuilder(
+                  valueListenable: Hive.box('profile').listenable(),
+                  builder: (BuildContext context, Box value, Widget? child) =>
+                      TopBar(
+                    title: value.get('name'),
+                    subtitle: 'Developer',
+                    color: Color(0xff0B3D2E),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'PHOTOGRAPHY',
+                  style: TextStyle(fontSize: 22, color: Colors.black),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                GridView.builder(
+                  itemCount: _photosData.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      crossAxisCount: 2),
+                  itemBuilder: (ctx, index) => Container(
+                      child: Image.network(
+                    _photosData[index].imgURL,
+                    fit: BoxFit.cover,
+                  )),
+                )
+              ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'PHOTOGRAPHY',
-              style: TextStyle(fontSize: 22, color: Colors.black),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            GridView.builder(
-              itemCount: _photosData.length,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisSpacing: 10, mainAxisSpacing: 10, crossAxisCount: 2),
-              itemBuilder: (ctx, index) => Container(
-                  child: Image.network(
-                _photosData[index].imgURL,
-                fit: BoxFit.cover,
-              )),
-            )
-          ],
+          ),
         ),
-      ),
-    ));
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed(ProfileScreen.routeName);
+          },
+          child: Icon(Icons.edit),
+        ));
   }
 }
